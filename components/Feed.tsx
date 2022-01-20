504eb93646bb7571fb29a942d7e6958111c50d52
chsunwoo1002 <chsunwoo1002@gmail.com>
2022-01-19 20:44:16 -0800

    add example input functionality

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import FlipVocaCard from "./card/FlipVocaCard";
 import { FeedProps } from "./types/feed";
 import { VocabularyType } from "./types/word";
@@ -10,6 +10,7 @@ import ExampleCard from "./card/ExampleCard";
 const Feed: React.FC<FeedProps> = ({ word }) => {
   const [isFavourite, setIsFavourite] = useState<Boolean>(false);
   const [isMemorized, setIsMemorized] = useState<Boolean>(false);
+  const [additionalExample, setAdditionalExample] = useState<string>("");
   const [examples, setExamples] = useState<Array<string>>([
     "apple is apple",
     "banana is banana",
@@ -23,10 +24,17 @@ const Feed: React.FC<FeedProps> = ({ word }) => {
     memorized: require("../assets/icons/done_all.png"),
     play: require("../assets/icons/play.png"),
     pause: require("../assets/icons/pause.png"),
+    add: require("../assets/icons/add.png"),
   };
 
   const switchButtonValue = () => {};
   const playSound = () => {};
+  const updateExampleString = (example: string) => {
+    setAdditionalExample(example);
+  };
+  const submitUpdateExample = () => {
+    setExamples((prev) => [additionalExample, ...prev]);
+  };
   return (
     <View style={Styles.feedContainer}>
       <View style={Styles.feedFlip}></View>
@@ -38,16 +46,27 @@ const Feed: React.FC<FeedProps> = ({ word }) => {
       </View>
       {examples.length > 0 ? (
         <View style={Styles.feedActiveExamplesContainer}>
-          {examples &&
+          <ExampleCard example={examples[0]} />
+          {/*examples &&
             examples.map((example, index) => {
               return <ExampleCard key={index} example={example} />;
-            })}
+            })*/}
         </View>
       ) : (
         <View style={Styles.feedDeactiveExampleContainer}>
           <Text> there is no examples here...please add them</Text>
         </View>
       )}
+      <View style={Styles.feedExampleContainer}>
+        <TextInput
+          style={Styles.feedExampleInput}
+          onChangeText={updateExampleString}
+          placeholder='add example here'
+          inlineImageLeft='search'
+          onSubmitEditing={submitUpdateExample}
+        />
+        <FeedButton onPress={submitUpdateExample} icon={feedIcons.add} />
+      </View>
     </View>
   );
 };
